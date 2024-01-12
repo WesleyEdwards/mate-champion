f2fe3b716e08c0fc1bef730d2afe03637415a7ce
WesleyEdwards <westheedwards@gmail.com>
2024-01-11 20:57:48 -0700

    fixing config

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -2,19 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
+function relativePath() {
+  return {
+    name: "relativePathChange",
+    transformIndexHtml(html: string) {
+      return html
+        .replace("/favicon.ico", "./favicon.ico")
+        .replace("/assets/", "./assets/");
+    },
+  };
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-    }),
-  ],
-  build: {
-    outDir: "../docs",
-  },
-  base: "./",
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ command, mode }) => {
+  const outDir = mode === "production" ? "build" : "../docs";
+  const plugins =
+    mode === "production"
+      ? [react(), checker({ typescript: true })]
+      : [react(), checker({ typescript: true }), relativePath()];
+
+  return {
+    plugins,
+    build: { outDir },
+    server: {
+      port: 3000,
+    },
+  };
 });
