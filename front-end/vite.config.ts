f91b974c7b60582e5407d0193b931443d43a98bc
WesleyEdwards <westheedwards@gmail.com>
2025-01-08 21:29:39 -0700

    add files to public

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import checker from "vite-plugin-checker";
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+import checker from "vite-plugin-checker"
 
 function relativePath() {
   return {
@@ -8,29 +8,28 @@ function relativePath() {
     transformIndexHtml(html: string) {
       return html
         .replace("/favicon.ico", "./favicon.ico")
-        .replace("/assets/", "./assets/");
-    },
-  };
+        .replace("/assets/", "./assets/")
+    }
+  }
 }
-
 
 // For deploying to github, move the mate-champion/assets directory to
 // just assets. This ensures that the path of the assets is correct.
 // Also make sure that the path to the js is correct
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const outDir = mode === "production" ? "build" : "../docs";
-  const assetsDir = mode === "production" ? "assets" : "mate-champion/assets";
+export default defineConfig(({command, mode}) => {
+  const outDir = mode === "production" ? "build" : "../docs"
+  const assetsDir = mode === "production" ? "assets" : "mate-champion/assets"
   const plugins =
     mode === "production"
-      ? [react(), checker({ typescript: true })]
-      : [react(), checker({ typescript: true }), relativePath()];
+      ? [react(), checker({typescript: true})]
+      : [react(), checker({typescript: true}), relativePath()]
 
   return {
     plugins,
-    build: { outDir, assetsDir },
+    build: {outDir, assetsDir},
     server: {
-      port: 3000,
-    },
-  };
-});
+      port: 3000
+    }
+  }
+})
